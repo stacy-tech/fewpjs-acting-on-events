aa03c9d66022dca3ac59e7274d4ff6f77a2cc2bd
stacy <stacydn23@gmail.com>
2021-05-16 16:34:03 -0400

    commit tree 46de9f65c2ec4b11d9b7b0d5a35bc406f25a0632

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Your code here
+// describs DODGER
+let dodger = document.getElementById("dodger");
+
+// function to move DODGER to the left
+function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+} 
+
+// function to move DODGER to the right 
+
+function moveDodgerRight() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left + 1}px`;
+    }
+} 
