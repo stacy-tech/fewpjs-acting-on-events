4d161d7b54a2c4ba98a304a0dc48e9c4fea269a1
stacy <stacydn23@gmail.com>
2021-05-16 16:45:29 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+// describs DODGER
+let dodger = document.getElementById("dodger");
+
+// function to move DODGER to the left
+function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+} 
+
+// function to move DODGER to the right 
+
+function moveDodgerRight() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left + 1}px`;
+    }
+} 
+
+//this ensures that the dodger doesn't escape off the left edge
+document.addEventListener('keydown', function(e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft()
+    }
+})
+
+// this emsures that the dodger doesn't escape off the right edge
+document.addEventListener('keydown', function(e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight()
+    }
+})
